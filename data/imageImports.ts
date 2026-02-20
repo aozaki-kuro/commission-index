d366bcd4597fdae813030bd468117ff1729e6a2a
Aozaki <i@aozaki.cc>
2026-02-20 21:43:46 +0800

    update(commission): new entry

--- data/imageImports.ts
+++ data/imageImports.ts
@@ -114,7 +114,7 @@ import A20251203 from '#images/webp/20251203.webp'
 import A20251223 from '#images/webp/20251223_MINK.webp'
 import A20260125 from '#images/webp/20260125_Gisyu.webp'
 import A20260208 from '#images/webp/20260208_Dorei.webp'
-import A20260221 from '#images/webp/20260221.webp'
+import A20260220 from '#images/webp/20260220_月.webp'
 
 export const imageImports = {
   '20201220_Yantaro': A20201220,
@@ -231,5 +231,5 @@ export const imageImports = {
   '20251223_MINK': A20251223,
   '20260125_Gisyu': A20260125,
   '20260208_Dorei': A20260208,
-  '20260221': A20260221,
+  '20260220_月': A20260220,
 }
