380f3b1f37fa033d73f2966b59f5d61c3b2a6fc1
Aozaki <i@aozaki.cc>
2026-03-14 23:03:32 +0800

    eslint: add formatter settings

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,24 @@ const eslintConfig = antfu({
   typescript: true,
   test: true,
   react: true,
+  formatters: {
+    /**
+     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+     * By default uses Prettier
+     */
+    css: true,
+    /**
+     * Format HTML files
+     * By default uses Prettier
+     */
+    html: true,
+    /**
+     * Format Markdown files
+     * Supports Prettier and dprint
+     * By default uses Prettier
+     */
+    // markdown: 'prettier',
+  },
 })
 
 eslintConfig.append({
